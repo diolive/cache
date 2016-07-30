f25b2500b6953cd360ec1ab9f6c9a12eb18a49ca
DioLive Studio <dio@live.ru>
2016-07-30 20:35:46 +0300

    Add npm and gulp. Move bootstrap to LESS. Change bower folder from wwwroot/lib to bower_components.

---
+++ DioLive.Cache/src/DioLive.Cache.WebUI/gulpfile.js
@@ -0,0 +1,23 @@
+﻿var gulp = require('gulp'),
+    rimraf = require('rimraf'),
+    less = require('gulp-less'),
+
+    project = require('./project.json');
+
+gulp.task('bootstrap:less', function () {
+    return gulp.src('Styles/bootstrap.less')
+        .pipe(less())
+        .pipe(gulp.dest('wwwroot/css'));
+});
+
+gulp.task('bootstrap:js', function () {
+    return gulp.src('bower_components/bootstrap/dist/js/bootstrap.js')
+        .pipe(gulp.dest('wwwroot/js'));
+});
+
+gulp.task('bootstrap:fonts', function () {
+    return gulp.src('bower_components/bootstrap/dist/fonts/*')
+        .pipe(gulp.dest('wwwroot/fonts'));
+});
+
+gulp.task('bootstrap', ['bootstrap:less', 'bootstrap:js', 'bootstrap:fonts']);
