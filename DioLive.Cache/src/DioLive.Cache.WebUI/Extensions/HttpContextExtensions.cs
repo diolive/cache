fd94dd55be6836adcdf0764e1fd1936c76ed34ab
DioLive Studio <dio@live.ru>
2016-08-14 12:07:02 +0300

    Extract GetCurrentCulture to extension method

---
+++ DioLive.Cache/src/DioLive.Cache.WebUI/Extensions/HttpContextExtensions.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Localization;
+
+namespace Microsoft.AspNetCore.Http
+{
+    public static class HttpContextExtensions
+    {
+        public static string GetCurrentCulture(this HttpContext httpContext)
+        {
+            return httpContext.Features.Get<IRequestCultureFeature>().RequestCulture.UICulture.Name;
+        }
+    }
+}
