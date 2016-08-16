43c3f63e51c01e47ecf40a71d8b9040ac45980df
DioLive Studio <dio@live.ru>
2016-08-16 11:08:42 +0300

    Remove old actions for category editing

--- DioLive.Cache/src/DioLive.Cache.WebUI/Controllers/CategoriesController.cs
+++ DioLive.Cache/src/DioLive.Cache.WebUI/Controllers/CategoriesController.cs
@@ -20,7 +20,6 @@ namespace DioLive.Cache.WebUI.Controllers
     public class CategoriesController : Controller
     {
         private const string Bind_Create = nameof(Category.Name);
-        private const string Bind_Edit = nameof(Category.Id) + "," + nameof(Category.Name);
 
         private readonly ApplicationDbContext _context;
         private readonly UserManager<ApplicationUser> _userManager;
@@ -102,74 +101,6 @@ namespace DioLive.Cache.WebUI.Controllers
                 return RedirectToAction(nameof(Index));
             }
             return View(category);
-        }
-
-        // GET: Categories/Edit/5
-        public async Task<IActionResult> Edit(int? id)
-        {
-            if (!id.HasValue)
-            {
-                return NotFound();
-            }
-
-            var category = await Get(id.Value);
-            if (category == null)
-            {
-                return NotFound();
-            }
-
-            if (!HasRights(category, ShareAccess.Categories))
-            {
-                return Forbid();
-            }
-
-            return View(category);
-        }
-
-        // POST: Categories/Edit/5
-        [HttpPost]
-        [ValidateAntiForgeryToken]
-        public async Task<IActionResult> Edit(int id, [Bind(Bind_Edit)] Category model)
-        {
-            if (id != model.Id)
-            {
-                return NotFound();
-            }
-
-            Category category = await Get(id);
-
-            if (category == null)
-            {
-                return NotFound();
-            }
-
-            if (!HasRights(category, ShareAccess.Categories))
-            {
-                return Forbid();
-            }
-
-            if (ModelState.IsValid)
-            {
-                category.Name = model.Name;
-
-                try
-                {
-                    await _context.SaveChangesAsync();
-                }
-                catch (DbUpdateConcurrencyException)
-                {
-                    if (!CategoryExists(model.Id))
-                    {
-                        return NotFound();
-                    }
-                    else
-                    {
-                        throw;
-                    }
-                }
-                return RedirectToAction(nameof(Index));
-            }
-            return View(model);
         }
 
         // POST: Categories/Update
