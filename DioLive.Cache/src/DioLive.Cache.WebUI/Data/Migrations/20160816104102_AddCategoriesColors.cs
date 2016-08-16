277fb3e7aa21b0a08a45665688be6d7a925dbac7
DioLive Studio <dio@live.ru>
2016-08-16 13:42:16 +0300

    Add colors to categories with randomly autogenerated values

---
+++ DioLive.Cache/src/DioLive.Cache.WebUI/Data/Migrations/20160816104102_AddCategoriesColors.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace DioLive.Cache.WebUI.Data.Migrations
+{
+    public partial class AddCategoriesColors : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<int>(
+                name: "Color",
+                table: "Category",
+                nullable: false,
+                defaultValueSql: "ABS(CHECKSUM(NEWID()) % 16777216)");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "Color",
+                table: "Category");
+        }
+    }
+}
