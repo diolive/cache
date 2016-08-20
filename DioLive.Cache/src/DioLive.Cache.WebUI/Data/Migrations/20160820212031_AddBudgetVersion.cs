b2f561d4957c883c5b47669bac621db13a389d7f
DioLive Studio <dio@live.ru>
2016-08-21 00:55:07 +0300

    Update database

---
+++ DioLive.Cache/src/DioLive.Cache.WebUI/Data/Migrations/20160820212031_AddBudgetVersion.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace DioLive.Cache.WebUI.Data.Migrations
+{
+    public partial class AddBudgetVersion : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<byte>(
+                name: "Version",
+                table: "Budget",
+                nullable: false,
+                defaultValue: (byte)1);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "Version",
+                table: "Budget");
+        }
+    }
+}
