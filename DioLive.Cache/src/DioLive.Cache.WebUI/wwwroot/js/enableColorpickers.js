c60d9fe2e819d8c2454ba20e62d68041bb4c881f
DioLive Studio <dio@live.ru>
2016-08-20 09:55:05 +0300

    Add color pickers

---
+++ DioLive.Cache/src/DioLive.Cache.WebUI/wwwroot/js/enableColorpickers.js
@@ -0,0 +1,12 @@
+﻿$(function () {
+    $('.colorpicker').colorpicker({
+        input: 'input[type="hidden"]',
+        format: 'hex'
+    }).on('changeColor', function (evt) {
+        $(this).colorpicker('disable');
+        $.post(CFG.updateCategoryUrl, { id: $(evt.target).data('id'), color: evt.color.toHex().substring(1) })
+            .always(function () {
+                $(this).colorpicker('enable');
+            });
+    });
+});
