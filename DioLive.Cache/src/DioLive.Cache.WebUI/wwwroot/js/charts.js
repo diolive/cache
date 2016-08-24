bad576476331c6e3cfa393f1e8064b9e3d9d976b
DioLive Studio <dio@live.ru>
2016-08-24 14:55:00 +0300

    Add Total chart

--- DioLive.Cache/src/DioLive.Cache.WebUI/wwwroot/js/charts.js
+++ DioLive.Cache/src/DioLive.Cache.WebUI/wwwroot/js/charts.js
@@ -1,4 +1,5 @@
 ﻿$(function () {
-    sunburst('.last-week', CFG.chartsSunburstDataUrl + '?days=7', 480, 350);
-    sunburst('.last-month', CFG.chartsSunburstDataUrl + '?days=30', 480, 350);
+    sunburst('#lastWeekChart', CFG.chartsSunburstDataUrl + '?days=7', 480, 350);
+    sunburst('#lastMonthChart', CFG.chartsSunburstDataUrl + '?days=30', 480, 350);
+    sunburst('#totalChart', CFG.chartsSunburstDataUrl, 480, 350);
 });
