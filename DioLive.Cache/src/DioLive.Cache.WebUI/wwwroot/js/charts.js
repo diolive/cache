43f0c55fff76132a091b63d08d76a53d1993bb94
DioLive Studio <dio@live.ru>
2016-08-28 23:28:53 +0300

    Increase monthly chart height

--- DioLive.Cache/src/DioLive.Cache.WebUI/wwwroot/js/charts.js
+++ DioLive.Cache/src/DioLive.Cache.WebUI/wwwroot/js/charts.js
@@ -2,5 +2,5 @@
     sunburst('#lastWeekChart', CFG.chartsSunburstDataUrl + '?days=7', 480, 350);
     sunburst('#lastMonthChart', CFG.chartsSunburstDataUrl + '?days=30', 480, 350);
     sunburst('#totalChart', CFG.chartsSunburstDataUrl, 480, 350);
-    stat('#statChart', CFG.chartsStatDataUrl + '?days=15&depth=30&step=2', 960, 500);
+    stat('#statChart', CFG.chartsStatDataUrl + '?days=15&depth=30&step=2', 960, 700);
 });
