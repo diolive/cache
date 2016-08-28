c52b5b23d64daba64b0ebdfc2e9aed4afe0125a5
DioLive Studio <dio@live.ru>
2016-08-28 23:06:29 +0300

    Add OpenBudget to DataHelper

--- DioLive.Cache/src/DioLive.Cache.WebUI/Models/DataHelper.cs
+++ DioLive.Cache/src/DioLive.Cache.WebUI/Models/DataHelper.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Threading.Tasks;
 
 using AutoMapper;
 
@@ -7,6 +8,7 @@ using DioLive.Cache.WebUI.Data;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Localization;
+using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Logging;
 
 namespace DioLive.Cache.WebUI.Models
@@ -36,5 +38,40 @@ namespace DioLive.Cache.WebUI.Models
         public IMapper Mapper { get; }
 
         public ILoggerFactory LoggerFactory { get; }
+
+        public async Task<LoadResult<Budget>> OpenCurrentBudget()
+        {
+            Guid? budgetId = this.CurrentBudgetId;
+            if (!budgetId.HasValue)
+            {
+                return LoadResult<Budget>.Fail(c => c.BadRequest());
+            }
+
+            return await OpenBudget(budgetId.Value);
+        }
+
+        public async Task<LoadResult<Budget>> OpenBudget(Guid id)
+        {
+            var budget = await this.Db.Budget
+                .Include(b => b.Shares)
+                .Include(b => b.Categories)
+                    .ThenInclude(c => c.Purchases)
+                .Include(b => b.Categories)
+                    .ThenInclude(c => c.Localizations)
+                .SingleOrDefaultAsync(b => b.Id == id);
+
+            if (budget == null)
+            {
+                return LoadResult<Budget>.Fail(c => c.NotFound());
+            }
+
+            string userId = this.UserManager.GetUserId(_httpContextAccessor.HttpContext.User);
+            if (!budget.HasRights(userId, ShareAccess.Categories))
+            {
+                return LoadResult<Budget>.Fail(c => c.Forbid());
+            }
+
+            return LoadResult<Budget>.Complete(budget);
+        }
     }
 }
