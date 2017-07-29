dccfa1d32a5655d7136ae3de1a85b93b48813655
DioLive Studio <dio@live.ru>
2017-07-29 23:03:28 +0400

    Add client-side tools and frameworks: NPM, Gulp, Bower, jQuery, Semantic UI

---
+++ src/DioLive.BlackMint.WebApp/gulpfile.js
@@ -0,0 +1,25 @@
+﻿var gulp = require("gulp");
+
+gulp.task("jquery",
+    function() {
+        return gulp
+            .src("bower_components/jquery/dist/jquery.min.js")
+            .pipe(gulp.dest("wwwroot/lib"));
+    });
+
+gulp.task("semantic-ui",
+    function() {
+        return gulp
+            .src("bower_components/semantic-ui/dist/semantic.min.*")
+            .pipe(gulp.dest("wwwroot/lib"));
+    });
+
+gulp.task("semantic-ui:themes",
+    function() {
+        return gulp
+            .src("bower_components/semantic-ui/dist/themes/**/*")
+            .pipe(gulp.dest("wwwroot/lib/themes"));
+    }
+);
+
+gulp.task("default", ["jquery", "semantic-ui", "semantic-ui:themes"]);
