dee62cfbb21856c87940fca3f8824ff0ae4ce108
DioLive Studio <dio@live.ru>
2019-08-23 13:08:58 +0300

    Fix dependencies, replace bower with yarn

--- src/DioLive.Cache.WebUI/gulpfile.js
+++ src/DioLive.Cache.WebUI/gulpfile.js
@@ -5,19 +5,19 @@ var gulp = require("gulp"),
 
 gulp.task("jquery:core",
     function() {
-        return gulp.src("bower_components/jquery/dist/jquery.js")
+        return gulp.src("node_modules/@bower_components/jquery/dist/jquery.js")
             .pipe(gulp.dest("wwwroot/js/lib"));
     });
 
 gulp.task("jquery-validation",
     function() {
-        return gulp.src("bower_components/jquery-validation/dist/*.js")
+        return gulp.src("node_modules/@bower_components/jquery-validation/dist/*.js")
             .pipe(gulp.dest("wwwroot/js/lib"));
     });
 
 gulp.task("jquery-validation-unobtrusive",
     function() {
-        return gulp.src("bower_components/jquery-validation-unobtrusive/jquery.validate.unobtrusive.js")
+        return gulp.src("node_modules/@bower_components/jquery-validation-unobtrusive/jquery.validate.unobtrusive.js")
             .pipe(gulp.dest("wwwroot/js/lib"));
     });
 
@@ -30,55 +30,55 @@ gulp.task("bootstrap:less",
 
 gulp.task("bootstrap:js",
     function() {
-        return gulp.src("bower_components/bootstrap/dist/js/bootstrap.js")
+        return gulp.src("node_modules/@bower_components/bootstrap/dist/js/bootstrap.js")
             .pipe(gulp.dest("wwwroot/js/lib"));
     });
 
 gulp.task("bootstrap:fonts",
     function() {
-        return gulp.src("bower_components/bootstrap/dist/fonts/*")
+        return gulp.src("node_modules/@bower_components/bootstrap/dist/fonts/*")
             .pipe(gulp.dest("wwwroot/fonts"));
     });
 
 gulp.task("bootstrap3-typeahead",
     function() {
-        return gulp.src("bower_components/bootstrap3-typeahead/bootstrap3-typeahead.js")
+        return gulp.src("node_modules/@bower_components/bootstrap3-typeahead/bootstrap3-typeahead.js")
             .pipe(gulp.dest("wwwroot/js/lib"));
     });
 
 gulp.task("bootstrap-datepicker:css",
     function() {
-        return gulp.src("bower_components/bootstrap-datepicker/dist/css/bootstrap-datepicker3.css")
+        return gulp.src("node_modules/@bower_components/bootstrap-datepicker/dist/css/bootstrap-datepicker3.css")
             .pipe(gulp.dest("wwwroot/css"));
     });
 
 gulp.task("bootstrap-datepicker:js",
     function() {
-        return gulp.src("bower_components/bootstrap-datepicker/dist/js/bootstrap-datepicker.js")
+        return gulp.src("node_modules/@bower_components/bootstrap-datepicker/dist/js/bootstrap-datepicker.js")
             .pipe(gulp.dest("wwwroot/js/lib"));
     });
 
 gulp.task("bootstrap-datepicker:locales",
     function() {
-        return gulp.src("bower_components/bootstrap-datepicker/dist/locales/*.js")
+        return gulp.src("node_modules/@bower_components/bootstrap-datepicker/dist/locales/*.js")
             .pipe(gulp.dest("wwwroot/js/lib/locales"));
     });
 
 gulp.task("bootstrap-slider:css",
     function() {
-        return gulp.src("bower_components/seiyria-bootstrap-slider/dist/css/bootstrap-slider.css")
+        return gulp.src("node_modules/@bower_components/seiyria-bootstrap-slider/dist/css/bootstrap-slider.css")
             .pipe(gulp.dest("wwwroot/css"));
     });
 
 gulp.task("bootstrap-slider:js",
     function() {
-        return gulp.src("bower_components/seiyria-bootstrap-slider/dist/bootstrap-slider.js")
+        return gulp.src("node_modules/@bower_components/seiyria-bootstrap-slider/dist/bootstrap-slider.js")
             .pipe(gulp.dest("wwwroot/js/lib"));
     });
 
 gulp.task("textarea-autosize",
     function() {
-        return gulp.src("bower_components/textarea-autosize/dist/jquery.textarea_autosize.js")
+        return gulp.src("node_modules/@bower_components/textarea-autosize/dist/jquery.textarea_autosize.js")
             .pipe(gulp.dest("wwwroot/js/lib"));
     });
 
@@ -98,25 +98,25 @@ gulp.task("bootstrap-override",
 
 gulp.task("bootstrap-colorpicker:css",
     function() {
-        return gulp.src("bower_components/bootstrap-colorpicker/dist/css/bootstrap-colorpicker.css")
+        return gulp.src("node_modules/@bower_components/bootstrap-colorpicker/dist/css/bootstrap-colorpicker.css")
             .pipe(gulp.dest("wwwroot/css"));
     });
 
 gulp.task("bootstrap-colorpicker:img",
     function() {
-        return gulp.src("bower_components/bootstrap-colorpicker/dist/img/**/*")
+        return gulp.src("node_modules/@bower_components/bootstrap-colorpicker/dist/img/**/*")
             .pipe(gulp.dest("wwwroot/img"));
     });
 
 gulp.task("bootstrap-colorpicker:js",
     function() {
-        return gulp.src("bower_components/bootstrap-colorpicker/dist/js/bootstrap-colorpicker.js")
+        return gulp.src("node_modules/@bower_components/bootstrap-colorpicker/dist/js/bootstrap-colorpicker.js")
             .pipe(gulp.dest("wwwroot/js/lib"));
     });
 
 gulp.task("d3:js",
     function() {
-        return gulp.src("bower_components/d3/d3.js")
+        return gulp.src("node_modules/@bower_components/d3/d3.js")
             .pipe(gulp.dest("wwwroot/js/lib"));
     });
 
@@ -127,21 +127,21 @@ gulp.task("d3charts:less",
             .pipe(gulp.dest("wwwroot/css"));
     });
 
-gulp.task("jquery", ["jquery:core", "jquery-validation", "jquery-validation-unobtrusive"]);
+gulp.task("jquery", gulp.series("jquery:core", "jquery-validation", "jquery-validation-unobtrusive"));
 
-gulp.task("bootstrap:core", ["bootstrap:less", "bootstrap:js", "bootstrap:fonts"]);
+gulp.task("bootstrap:core", gulp.series("bootstrap:less", "bootstrap:js", "bootstrap:fonts"));
 
 gulp.task("bootstrap-datepicker",
-    ["bootstrap-datepicker:css", "bootstrap-datepicker:js", "bootstrap-datepicker:locales"]);
+    gulp.series("bootstrap-datepicker:css", "bootstrap-datepicker:js", "bootstrap-datepicker:locales"));
 
-gulp.task("bootstrap-slider", ["bootstrap-slider:css", "bootstrap-slider:js"]);
+gulp.task("bootstrap-slider", gulp.series("bootstrap-slider:css", "bootstrap-slider:js"));
 
 gulp.task("bootstrap-colorpicker",
-    ["bootstrap-colorpicker:css", "bootstrap-colorpicker:img", "bootstrap-colorpicker:js"]);
+    gulp.series("bootstrap-colorpicker:css", "bootstrap-colorpicker:img", "bootstrap-colorpicker:js"));
 
 gulp.task("bootstrap",
-    ["bootstrap:core", "bootstrap3-typeahead", "bootstrap-datepicker", "bootstrap-slider", "bootstrap-colorpicker"]);
+    gulp.series("bootstrap:core", "bootstrap3-typeahead", "bootstrap-datepicker", "bootstrap-slider", "bootstrap-colorpicker"));
 
-gulp.task("d3", ["d3:js", "d3charts:less"]);
+gulp.task("d3", gulp.series("d3:js", "d3charts:less"));
 
-gulp.task("default", ["jquery", "bootstrap", "textarea-autosize", "site:less", "bootstrap-override", "d3"]);
+gulp.task("default", gulp.series("jquery", "bootstrap", "textarea-autosize", "site:less", "bootstrap-override", "d3"));
